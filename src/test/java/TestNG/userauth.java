a7264d1eb7d114dc0c75a9153e0de49f6242a0d2
Kashish <trivedikashish@gmail.com>
2019-05-27 18:06:35 +0530

    updated chrome driver

--- src/test/java/TestNG/userauth.java
+++ src/test/java/TestNG/userauth.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,8 +15,10 @@ public class userauth {
 	@BeforeMethod
 	public void launch() {
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver","geckodriver.exe");
 		  driver = new ChromeDriver();
-		  driver.get("http://192.168.56.101:3001");
+         //FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://192.168.56.101:3001");
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 	}
