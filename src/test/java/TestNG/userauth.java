bb3471aff57998bef3e5370f289d0fc0da914446
Kashish <trivedikashish@gmail.com>
2019-05-27 19:20:50 +0530

    added chrome options -no-sandbox

--- src/test/java/TestNG/userauth.java
+++ src/test/java/TestNG/userauth.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -21,6 +22,9 @@ public class userauth {
 		driver.get("http://192.168.56.101:3001");
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		  ChromeOptions ChromeOptions = new ChromeOptions();
+		  ChromeOptions.addArguments("window-size=1024,768", "--no-sandbox");
+		  //driver = new ChromeDriver(ChromeOptions);
 	}
   @Test
   public void verifyauth() {
@@ -29,8 +33,8 @@ public class userauth {
 	  driver.findElement(By.xpath("//input[@class='btn btn-primary']")).click();
   }
   
-  @AfterMethod
-  public void close() {
-	  driver.close();
-	}
+ // @AfterMethod
+  //public void close() {
+	//  driver.close();
+	//}
 }
